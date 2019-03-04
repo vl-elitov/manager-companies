f5a2439fbdd79e135a8870f2ea934788acc7ec74
Vladimir Anufriev <anufrievv.vvladimir@gmail.com>
2019-03-04 17:46:15 +0300

    Added "go back" button in create view

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Create company</div>
+                    <div class="panel-heading">Create company <a href="/dashboard" class="pull-right btn btn-default btn-xs">Go Back</a></div>
 
                     <div class="panel-body">
                         {!!Form::open(['action' => 'CompaniesController@store','method' => 'POST'])!!}
